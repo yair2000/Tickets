2d59151ba9f4ffbb4d741b992650dd35cb7e0c94
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-03-03 14:40:17 +0200

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -29,7 +29,7 @@ function Header(){
             <li><Link to="/login"><FaSignInAlt />Login</Link></li>
             <li><Link to="/register"><FaUser />Register</Link></li>
           </>
-          )};
+          )}
         </ul>
     </header>
   )

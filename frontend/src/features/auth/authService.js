16f1b772eac8f709d58b95cdde6547edcb057a53
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-03-03 19:09:50 +0200

    Logging out now works properly

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -20,7 +20,7 @@ const login = async(userData) =>{
   return response.data;
 }
 
-const logout = localStorage.removeItem("user");
+const logout = () => localStorage.removeItem("user");
 
 const authService = {register, login, logout}
 

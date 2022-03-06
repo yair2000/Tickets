2c69affba9ee0f63c835555ead500ee2f3a62e6c
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-03-06 12:38:00 +0200

    Update ticketController.js

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -3,6 +3,28 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const Ticket = require("../models/Ticket");
 
+const createTicket = asyncHandler(async (req, res) =>{
+   const {product, description} = req.body
+   if(!product || !description){
+      res.status(400);
+      throw new Error("Enter A Product And Provide Its Description");
+   }
+
+   const user = await User.findById(req.user.id);
+   if(!user){
+      res.status(401);
+      throw new Error("User Not Found");
+   }
+   const ticket = await Ticket.create({
+       product,
+       description,
+       user: req.user.id,
+       status: "New"
+   });
+
+   res.status(201).json(ticket);
+});
+
 const getTicket = asyncHandler(async (req, res) =>{
    const user = await User.findById(req.user.id);
    if(!user){
@@ -21,24 +43,14 @@ const getTicket = asyncHandler(async (req, res) =>{
    res.status(200).json(ticket);
 });
 
-const deleteTicket = asyncHandler(async (req, res) =>{
+const getTickets = asyncHandler(async (req, res) =>{
    const user = await User.findById(req.user.id);
    if(!user){
       res.status(401);
       throw new Error("User Not Found");
    }
-   const ticket = await Ticket.findById(req.params.id);
-   if(!ticket){
-      res.status(404);
-      throw new Error("Ticket Not Found");
-   }
-   if(ticket.user.toString() !== req.user.id){
-      res.status(401);
-      throw new Error("Not Authorized For This Action");
-   }
-   await ticket.remove();
-
-   res.status(200).json({success: true});
+   const tickets = await Ticket.find({user: req.user.id});
+   res.status(200).json(tickets);
 });
 
 const updateTicket = asyncHandler(async (req, res) =>{
@@ -64,36 +76,24 @@ const updateTicket = asyncHandler(async (req, res) =>{
    res.status(200).json(updatedTicket);
 });
 
-const getTickets = asyncHandler(async (req, res) =>{
+const deleteTicket = asyncHandler(async (req, res) =>{
    const user = await User.findById(req.user.id);
    if(!user){
       res.status(401);
       throw new Error("User Not Found");
    }
-   const tickets = await Ticket.find({user: req.user.id});
-   res.status(200).json(tickets);
-});
-
-const createTicket = asyncHandler(async (req, res) =>{
-   const {product, description} = req.body
-   if(!product || !description){
-      res.status(400);
-      throw new Error("Enter A Product And Provide Its Description");
+   const ticket = await Ticket.findById(req.params.id);
+   if(!ticket){
+      res.status(404);
+      throw new Error("Ticket Not Found");
    }
-
-   const user = await User.findById(req.user.id);
-   if(!user){
+   if(ticket.user.toString() !== req.user.id){
       res.status(401);
-      throw new Error("User Not Found");
+      throw new Error("Not Authorized For This Action");
    }
-   const ticket = await Ticket.create({
-       product,
-       description,
-       user: req.user.id,
-       status: "New"
-   });
+   await ticket.remove();
 
-   res.status(201).json(ticket);
+   res.status(200).json({success: true});
 });
 
 module.exports = {getTicket, deleteTicket, updateTicket, getTickets, createTicket}

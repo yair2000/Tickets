2a9fa19127e4b7f9688adfaad5379e943c07c590
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-04-17 18:00:16 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as ReactDOMClient from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 import { store } from './app/store';

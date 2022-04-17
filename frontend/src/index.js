2a5df277470d07fcc47be990340bafa7e35b4835
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-04-17 18:01:15 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,11 @@ import './index.css'
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Provider store={store}>
-  <App/>
-</Provider>
+<React.StrictMode>
+  <Provider store={store}>
+    <App/>
+  </Provider>
+</React.StrictMode>
 );
